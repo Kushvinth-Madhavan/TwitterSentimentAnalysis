5943d3e646e47cf36135dfb61a6c4bed8a10bf1e
Kushvinth-Madhavan <kushvinth.m@gmail.com>
2025-01-29 11:29:13 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+setup(
+    name="twitter-sentiment-analysis",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        'tweepy==4.14.0',
+        'textblob==0.17.1',
+        'python-dotenv==1.0.0',
+        'pandas==2.0.0',
+        'nltk==3.8.1',
+    ],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A Twitter sentiment analysis tool using TextBlob",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/twitter-sentiment-analysis",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
